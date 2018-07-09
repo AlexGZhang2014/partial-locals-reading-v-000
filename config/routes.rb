558beee517e2c5466ad7bd6f2d470b76aa52f66b
Alex Zhang <alexgzhang92@gmail.com>
2018-07-09 22:10:29 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :author, only: [:show, :index]
-  resources :post, only: [:show, :index]
+  resources :authors, only: [:show, :index]
+  resources :posts, only: [:show, :index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
